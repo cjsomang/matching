32bd436c99cbe42c035e7d7aa1a59293c9a4dd39
cjsomang <cjsomang@naver.com>
2025-07-11 18:08:00 +0900

    DB change to MYSQL'

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': env('DB_ENGINE'),
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
