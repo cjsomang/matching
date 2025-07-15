a4170c653266da5b60a930fb43d37ec89a48d7db
cjsomang <cjsomang@naver.com>
2025-07-15 18:12:53 +0900

    csp block changed

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
